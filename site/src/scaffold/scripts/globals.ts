e9768dd062d03e800920218c60ff4384feb989a2
Will Bjorn <wibjorn@microsoft.com>
2021-06-11 10:12:07 -0700

    move to script type module

--- site/src/scaffold/scripts/globals.ts
+++ site/src/scaffold/scripts/globals.ts
@@ -3,11 +3,3 @@ export const document = window.document;
 export const localStorage = window.localStorage;
 export const history = window.history;
 export const location = window.location;
-
-export const contentLoaded = new Promise<void>(resolve => {
-	if (document.readyState === 'loading') {
-		document.addEventListener('DOMContentLoaded', () => resolve());
-	} else {
-		resolve();
-	}
-});
