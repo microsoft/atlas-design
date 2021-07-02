8c014e5268a684d158eaade25a38cb057da1e698
Joel Watson <v-joelwatson@microsoft.com>
2021-07-02 13:23:34 -0700

    removed inline css and simplified CSP

--- plugins/parcel-transformer-markdown-html/lib/index.js
+++ plugins/parcel-transformer-markdown-html/lib/index.js
@@ -25,7 +25,7 @@ let filterIds = 0;
 function createFilterableCodeBlock(code, language, displayName) {
 	filterIds++;
 	return `
-	<div class="code-block margin-top-xs" style="min-height: 30vh">
+	<div class="code-block margin-top-xs code-block-min-height">
 		<div class="code-block-header">
 			<span class="code-block-header-language" data-hljs-language="${language}">${displayName}</span>
 			<input
@@ -34,7 +34,7 @@ function createFilterableCodeBlock(code, language, displayName) {
 			placeholder="Filter ..."
 			type="search" />
 		</div>
-		<div class="code-block-body" style="max-height: 30vh; overflow-y: scroll;">
+		<div class="code-block-body code-bock-body-max-height">
 			<pre><code data-code-filter-code="${filterIds}">${code}</code></pre>
 		</div>
 	</div>`;
