504b8973d0512ebe192e2b9bf03a8b61a5a9dfc8
Austin Akers <austinakers@microsoft.com>
2022-01-18 17:19:31 -0800

    created togglable sidebar for tablet view and modified layout

--- site/src/scaffold/index.ts
+++ site/src/scaffold/index.ts
@@ -7,3 +7,12 @@ initTheme();
 initPopovers(document.body);
 handleCodeFilters();
 handleFigmaFullScreenRequest();
+
+const navbarMobile = document.querySelector('#nav-mobile');
+const navbarToggle = document.querySelector('#navbar-toggle');
+
+const toggleSidebar = () => {
+	navbarMobile?.classList.toggle('open');
+};
+
+navbarToggle?.addEventListener('click', toggleSidebar);
