1853b0ac662577ceddc1eadaa7656fc5c074c7af
Olga Lipskaya <ollipska@microsoft.com>
2022-01-20 14:01:58 -0800

    Small rename; temp removing the documentation portion

--- site/src/scaffold/scripts/popover.ts
+++ site/src/scaffold/scripts/popover.ts
@@ -29,7 +29,7 @@ export function initPopovers(container: HTMLElement) {
 					closePopovers();
 				}
 
-				if (event.type === 'click' && event.target.hasAttribute('data-close')) {
+				if (event.type === 'click' && event.target.hasAttribute('data-popover-close')) {
 					closePopovers();
 				}
 			};
