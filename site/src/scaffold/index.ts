25b8879953de1c3f371e47f7b381531154862f24
Will Bjorn <wibjorn@microsoft.com>
2022-03-25 10:57:08 -0700

    update

--- site/src/scaffold/index.ts
+++ site/src/scaffold/index.ts
@@ -2,8 +2,9 @@ import { handleCodeFilters } from './scripts/code-filter';
 import { handleFigmaFullScreenRequest } from './scripts/figma-embed';
 import { initTheme } from './scripts/theming';
 
-import '@microsoft/atlas-js/src/index';
+import { initPopover } from '@microsoft/atlas-js/src/index';
 
 initTheme();
+initPopover();
 handleCodeFilters();
 handleFigmaFullScreenRequest();
