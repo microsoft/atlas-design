5053cd17253b29bd72c749b505e5129d0c71ed7d
Will Bjorn <wibjorn@microsoft.com>
2022-04-11 14:56:50 -0700

    update

--- integration/visual-diff/visual-diff.spec.ts
+++ integration/visual-diff/visual-diff.spec.ts
@@ -8,7 +8,7 @@ for (const pageConfig of pages) {
 
 	test(`"${pageConfig.pathname}"`, async ({ page }) => {
 		await page.goto(pageConfig.pathname);
-		const pathname = pageConfig.name.replace(/\s|\\|\//g, '_');
+		const pathname = './visual-diff/screenshots/' + pageConfig.name.replace(/\s|\\|\//g, '_');
 		expect
 			.soft(await page.screenshot({ path: pathname, type: 'png', ...screenshotSettings }))
 			.toMatchSnapshot();
