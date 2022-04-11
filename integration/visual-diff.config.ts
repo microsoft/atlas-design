9e16a928bf65719b8c3c48c5cef08a3dbd29b1a5
Will Bjorn <wibjorn@microsoft.com>
2022-04-11 15:49:09 -0700

    update

--- integration/visual-diff.config.ts
+++ integration/visual-diff.config.ts
@@ -43,39 +43,44 @@ const config: PlaywrightTestConfig = {
 	/* Configure projects for major browsers */
 	projects: [
 		{
-			name: 'chromium',
+			name: 'Widescreen Chromium',
 			use: {
-				...devices['Desktop Chrome']
+				browserName: 'chromium',
+				// Test against Chrome Beta channel.
+				viewport: {
+					width: 1920,
+					height: 1080
+				}
 			}
+		},
+		{
+			name: 'Desktop Chromium',
+			use: {
+				browserName: 'chromium',
+				viewport: {
+					width: 1152,
+					height: 648
+				}
+			}
+		},
+		{
+			name: 'Tablet Chromium',
+			use: {
+				browserName: 'chromium',
+				viewport: {
+					width: 768,
+					height: 600
+				}
+			}
+		},
+		{
+			name: 'Android Chrome',
+			use: devices['Pixel 5']
+		},
+		{
+			name: 'Mobile Safari',
+			use: devices['iPhone 13']
 		}
-
-		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: {
-		//     ...devices['Pixel 5'],
-		//   },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: {
-		//     ...devices['iPhone 12'],
-		//   },
-		// },
-
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: {
-		//     channel: 'msedge',
-		//   },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: {
-		//     channel: 'chrome',
-		//   },
-		// },
 	],
 
 	/* Folder for test artifacts such as screenshots, videos, traces, etc. */
