563bc7e3067a0b6ae4e24547e2c24bff9c54fda6
Will Bjorn <wibjorn@microsoft.com>
2022-04-13 08:46:38 -0700

    wip

--- integration/visual-diff/visual-diff.spec.ts
+++ integration/visual-diff/visual-diff.spec.ts
@@ -9,15 +9,31 @@ test.describe.configure({ mode: 'parallel' });
 
 // ((route: Route, request: Request) => void)
 
+const axilliaryThemes = ['dark', 'high-contrast']; // light is always included
+const includeAllThemes = process.env.argv?.includes('--full-diff');
+
 for (const pageConfig of pages) {
 	const screenshotSettings = pageConfig?.options?.screenshot || {};
 
 	test(`"${pageConfig.pathname}"`, async ({ page, context }) => {
 		await Promise.all(pageConfig.routes.map(r => context.route(r.url, r.handler, r.options)));
 
-		// await Promise.all(pageConfig.routes.map(route => ));
-		await page.goto(pageConfig.pathname);
-		// const pathname = '' + pageConfig.name.replace(/\s|\\|\//g, '_') + '.png';
-		expect(await page.screenshot({ ...screenshotSettings })).toMatchSnapshot();
+		const moreThemes = includeAllThemes ? axilliaryThemes : [];
+
+		const imageName = transformNameToFilePath(pageConfig.name);
+
+		for (const theme in ['light', ...moreThemes]) {
+			// await Promise.all(pageConfig.routes.map(route => ));
+			await page.goto(pageConfig.pathname);
+			// const pathname = '' + pageConfig.name.replace(/\s|\\|\//g, '_') + '.png';
+			await page.screenshot({
+				...screenshotSettings,
+				path: `./visual-diff/snapshots/${imageName}__light__.png`
+			});
+		}
 	});
 }
+
+const transformNameToFilePath = (name: string) => {
+	return name.replace(/(\s|\\|\/)/g, '_');
+};
