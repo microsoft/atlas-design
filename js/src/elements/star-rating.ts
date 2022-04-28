9ef9b961141e21e5f2a80c59a84098bba6e331f1
Ming Ho <ming.ho@microsoft.com>
2022-04-27 19:55:02 -0700

    Update labels

--- js/src/elements/star-rating.ts
+++ js/src/elements/star-rating.ts
@@ -75,6 +75,7 @@ starRatingTemplate.innerHTML = `
 	
 	input:focus-visible + label {
 		outline: 3px dashed;
+		border-radius: 0.5em;
 	}
 	
 	input[value="1"]:checked ~ #alert #label-1,
@@ -118,46 +119,46 @@ starRatingTemplate.innerHTML = `
 
 	<div id="input-container">
 		<input part="visually-hidden" type="radio" value="1" id="radio-1" />
-		<label id="star-1" for="radio-1">
+		<label for="radio-1">
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" style="overflow: visible" >
 				<path d="M7.1939 2.10167C7.52403 1.43275 8.47789 1.43274 8.80802 2.10167L10.3291 5.18372L13.7304 5.67795C14.4685 5.78522 14.7633 6.69239 14.2291 7.21307L11.768 9.61212L12.349 12.9996C12.4751 13.7348 11.7034 14.2955 11.0431 13.9484L8.00096 12.349L4.95879 13.9484C4.29853 14.2955 3.52684 13.7348 3.65294 12.9996L4.23394 9.61212L1.77277 7.21307C1.23861 6.69239 1.53336 5.78522 2.27156 5.67795L5.67281 5.18372L7.1939 2.10167Z"/>
 			</svg>
 		</label>
 
 		<input part="visually-hidden" type="radio" value="2" id="radio-2" />
-		<label id="star-2" for="radio-2">
+		<label for="radio-2">
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" style="overflow: visible" >
 				<path d="M7.1939 2.10167C7.52403 1.43275 8.47789 1.43274 8.80802 2.10167L10.3291 5.18372L13.7304 5.67795C14.4685 5.78522 14.7633 6.69239 14.2291 7.21307L11.768 9.61212L12.349 12.9996C12.4751 13.7348 11.7034 14.2955 11.0431 13.9484L8.00096 12.349L4.95879 13.9484C4.29853 14.2955 3.52684 13.7348 3.65294 12.9996L4.23394 9.61212L1.77277 7.21307C1.23861 6.69239 1.53336 5.78522 2.27156 5.67795L5.67281 5.18372L7.1939 2.10167Z"/>
 			</svg>
 		</label>
 
 		<input part="visually-hidden" type="radio" value="3" id="radio-3" />
-		<label id="star-3" for="radio-3">
+		<label for="radio-3">
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" style="overflow: visible" >
 				<path d="M7.1939 2.10167C7.52403 1.43275 8.47789 1.43274 8.80802 2.10167L10.3291 5.18372L13.7304 5.67795C14.4685 5.78522 14.7633 6.69239 14.2291 7.21307L11.768 9.61212L12.349 12.9996C12.4751 13.7348 11.7034 14.2955 11.0431 13.9484L8.00096 12.349L4.95879 13.9484C4.29853 14.2955 3.52684 13.7348 3.65294 12.9996L4.23394 9.61212L1.77277 7.21307C1.23861 6.69239 1.53336 5.78522 2.27156 5.67795L5.67281 5.18372L7.1939 2.10167Z"/>
 			</svg>
 		</label>
 
 		<input part="visually-hidden" type="radio" value="4" id="radio-4" />
-		<label id="star-4" for="radio-4">
+		<label for="radio-4">
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" style="overflow: visible" >
 				<path d="M7.1939 2.10167C7.52403 1.43275 8.47789 1.43274 8.80802 2.10167L10.3291 5.18372L13.7304 5.67795C14.4685 5.78522 14.7633 6.69239 14.2291 7.21307L11.768 9.61212L12.349 12.9996C12.4751 13.7348 11.7034 14.2955 11.0431 13.9484L8.00096 12.349L4.95879 13.9484C4.29853 14.2955 3.52684 13.7348 3.65294 12.9996L4.23394 9.61212L1.77277 7.21307C1.23861 6.69239 1.53336 5.78522 2.27156 5.67795L5.67281 5.18372L7.1939 2.10167Z"/>
 			</svg>
 		</label>
 
 		<input part="visually-hidden" type="radio" value="5" id="radio-5" />
-		<label id="star-5" for="radio-5">
+		<label for="radio-5">
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" style="overflow: visible" >
 				<path d="M7.1939 2.10167C7.52403 1.43275 8.47789 1.43274 8.80802 2.10167L10.3291 5.18372L13.7304 5.67795C14.4685 5.78522 14.7633 6.69239 14.2291 7.21307L11.768 9.61212L12.349 12.9996C12.4751 13.7348 11.7034 14.2955 11.0431 13.9484L8.00096 12.349L4.95879 13.9484C4.29853 14.2955 3.52684 13.7348 3.65294 12.9996L4.23394 9.61212L1.77277 7.21307C1.23861 6.69239 1.53336 5.78522 2.27156 5.67795L5.67281 5.18372L7.1939 2.10167Z"/>
 			</svg>
 		</label>
 
 		<span id="alert">
-			<span id="label-1"><slot name="label-1"></slot></span>
-			<span id="label-2"><slot name="label-2"></slot></span>
-			<span id="label-3"><slot name="label-3"></slot></span>
-			<span id="label-4"><slot name="label-4"></slot></span>
-			<span id="label-5"><slot name="label-5"></slot></span>
+			<label for="radio-1"><span id="label-1"><slot name="label-1"></slot></span></label>
+			<label for="radio-2"><span id="label-2"><slot name="label-2"></slot></span></label>
+			<label for="radio-3"><span id="label-3"><slot name="label-3"></slot></span></label>
+			<label for="radio-4"><span id="label-4"><slot name="label-4"></slot></span></label>
+			<label for="radio-5"><span id="label-5"><slot name="label-5"></slot></span></label>
 		</span>
 	</div>
 </fieldset>
