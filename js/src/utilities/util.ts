8dccf01f2b1cd9db3956ce0c957764d3191eaea9
Ming Ho <ming.ho@microsoft.com>
2022-04-29 13:37:00 -0700

    Revert

--- js/src/utilities/util.ts
+++
@@ -1,9 +0,0 @@
-let nextId = 0;
-
-export function generateElementId() {
-	return `ax-${nextId++}`;
-}
-
-export function parseValue(str: string | null): number {
-	return Math.max(0, Math.min(parseInt(str || '0'), 5));
-}
