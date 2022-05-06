fab7b3400c3e912ebea5a1d4c910c5c89fd9e24c
Ming Ho <ming.ho@microsoft.com>
2022-05-06 12:03:34 -0700

    Add initial version

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,2 +1,4 @@
 export * from './behaviors/popover';
+export * from './behaviors/form';
+export * from './behaviors/tag-inputs';
 export * from './elements/star-rating';
--- site/src/scaffold/index.ts
+++ site/src/scaffold/index.ts
@@ -2,9 +2,10 @@ import { handleCodeFilters } from './scripts/code-filter';
 import { handleFigmaFullScreenRequest } from './scripts/figma-embed';
 import { initTheme } from './scripts/theming';
 
-import { initPopover } from '@microsoft/atlas-js/src/index';
+import { initPopover, initTagInputs } from '@microsoft/atlas-js/src/index';
 
 initTheme();
 initPopover();
+initTagInputs();
 handleCodeFilters();
 handleFigmaFullScreenRequest();
