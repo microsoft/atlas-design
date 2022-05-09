d16172bddac988d27274f83ade878e72f4bcc9b2
Ming Ho <ming.ho@microsoft.com>
2022-05-09 14:16:47 -0700

    Add tag input delete button

--- js/src/behaviors/tag-inputs.ts
+++ js/src/behaviors/tag-inputs.ts
@@ -1,31 +1,70 @@
 export function initTagInputs(container: HTMLElement = document.body) {
+	const deleteTagLabel = 'Delete {tagName} tag';
+	let tagCount = 0;
 	container.addEventListener(
 		'click',
 		(event: Event) => {
 			const targetTagContainer =
 				event.target instanceof Element &&
 				(event.target.closest('div.control')?.querySelector('input.tag-input') as HTMLInputElement);
+
+			const tagHolder = document.querySelector('.tag-input-holder');
 			if (!targetTagContainer) {
 				return;
 			}
+			if (!event.target.classList.contains('add-tags-button')) {
+				return;
+			}
 
-			targetTagContainer.value += 'Tags,';
-			const tagHolder = document.querySelector('.tag-input-holder');
 			tagHolder?.classList.add('padding-block-xxs');
-			const tag = document.createElement('div');
-			tag.classList.add('tag');
-			tag.style.height = 'auto';
-			tag.classList.add(
+			const newTag = document.createElement('div');
+			newTag.id = `Tag${tagCount++}`;
+			newTag.setAttribute('role', 'listitem');
+			newTag.classList.add('tag');
+			newTag.style.height = 'auto';
+			newTag.classList.add(
 				'display-inline-flex',
 				'background-color-body-medium',
 				'padding-inline-xxs',
 				'margin-inline-xxs'
 			);
-			tag.innerText = 'tag';
+			newTag.innerText = newTag.id;
+			targetTagContainer.value += `${newTag.id},`;
+
+			const deleteButton = document.createElement('button');
+			deleteButton.classList.add('delete', 'has-inner-focus', 'border-none');
+			deleteButton.type = 'button';
+			deleteButton.setAttribute(
+				'aria-label',
+				deleteTagLabel.replace('{tagName}', newTag.innerText)
+			);
+			deleteButton.innerText = 'X';
+			deleteButton.addEventListener('click', e => {
+				// make sure the event isn't coming from the parent input!
+				if (document.activeElement?.classList.contains('delete')) {
+					deleteTag(e, newTag.id);
+				}
+			});
+			newTag.insertAdjacentElement('beforeend', deleteButton);
+
 			if (tagHolder) {
-				tagHolder.append(tag);
+				tagHolder.append(newTag);
 			}
 		},
 		true
 	);
+
+	function deleteTag(e: Event, tagId: string) {
+		const deleteButton = e.currentTarget as HTMLElement;
+		if (!(deleteButton instanceof HTMLButtonElement)) {
+			return;
+		}
+		const input = document.querySelector('input.tag-input') as HTMLInputElement;
+		input.value = input.value
+			.split(',')
+			.filter(s => s !== tagId)
+			.join(',');
+		input.dispatchEvent(new Event('change', { bubbles: true }));
+		deleteButton.parentElement!.remove();
+	}
 }
