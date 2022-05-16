6c37f2b929d9660a6e291913870ab0a8ebf0dbba
Ming Ho <ming.ho@microsoft.com>
2022-05-16 16:16:56 -0700

    Update documentation select example

--- site/src/scaffold/scripts/form-submit.ts
+++ site/src/scaffold/scripts/form-submit.ts
@@ -2,11 +2,12 @@ export function handleMockFormSubmit() {
 	document.querySelectorAll('form')?.forEach(form => {
 		form.addEventListener('submit', function (e) {
 			e.preventDefault();
-			submitAlert();
+			const formData = new FormData(form);
+			submitAlert(formData);
 		});
 	});
 }
 
-function submitAlert() {
-	alert('The form is being submitted');
+function submitAlert(formData: FormData) {
+	alert(`The form is being submitted, ${JSON.stringify(Object.fromEntries(formData), null, 2)}`);
 }
