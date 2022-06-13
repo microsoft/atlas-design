785900327ff1941edc023b97c2f93f78762a48e9
Ming Ho <ming.ho@microsoft.com>
2022-06-13 00:18:03 -0700

    Update js index.ts

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,4 +1,4 @@
 export * from './behaviors/popover';
-export * from './behaviors/form';
 export * from './behaviors/tag-inputs';
+export * from './elements/form-behavior';
 export * from './elements/star-rating';
