94e277d728f31bc23c15101c102cc89faf66408b
Will Bjorn <wibjorn@microsoft.com>
2022-10-28 11:09:27 -0700

    update

--- integration/tasks/visual-diff-local.ps1
+++ integration/tasks/visual-diff-local.ps1
@@ -8,7 +8,7 @@ Set-Location ../
 Remove-Item -Path "./integration/visual-diff/screenshots/" -Recurse -Force -ErrorAction Ignore
 
 # checkout branch that will be used for baseline screenshots
-git checkout wbb/visual-diff-local
+git checkout main
 
 # take baseline screenshots
 npm run screenshots:light --workspace=@microsoft/atlas-integration
