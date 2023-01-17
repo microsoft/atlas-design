259f8d9895fbaddc1cf8b4bff2efd0c5ae981b6a
James Benson <92340273+jim-benson@users.noreply.github.com>
2023-01-17 11:11:29 -0800

    update functionality

--- js/src/behaviors/tabs.ts
+++ js/src/behaviors/tabs.ts
@@ -8,6 +8,7 @@
 export function initTabs() {
 	const container = document.getElementById('tab-test') as HTMLElement;
 
+	updateTabNav(container);
 	initTabNavClickListeners(container);
 	initTabControlClickListeners(container);
 }
@@ -23,17 +24,41 @@ function initTabNavClickListeners(container: HTMLElement = document.body) {
 		button.addEventListener('click', (e: Event) => {
 			e.preventDefault();
 			const b = e.target as HTMLButtonElement;
-			const currentActiveTab = container.querySelector('.is-active');
-			const currentButton = currentActiveTab?.firstChild as HTMLButtonElement;
-			const index = currentButton?.dataset.tabControl;
-			console.log(currentActiveTab, index);
+			// get current activated tab
+			const currentActiveButton = container.querySelector('.is-active')
+				?.firstElementChild as HTMLButtonElement;
+			const index = parseInt(currentActiveButton.dataset.tabControl as string);
 
+			// update tab
 			if (b.className.includes('tab-previous')) {
-				console.log('previous');
+				updateTabState(container, `${index - 1}`);
+				updateTabItemState(container, `${index - 1}`);
 			} else if (b.className.includes('tab-next')) {
-				console.log('next');
+				updateTabState(container, `${index + 1}`);
+				updateTabItemState(container, `${index + 1}`);
 			}
+			updateTabNav(container);
 		});
+	}
+}
+
+function updateTabNav(container: HTMLElement = document.body) {
+	const buttons = Array.from(container.querySelectorAll('.tab-control')) as HTMLButtonElement[];
+	const currentActiveButton = container.querySelector('.is-active')
+		?.firstElementChild as HTMLButtonElement;
+	const index = parseInt(currentActiveButton.dataset.tabControl as string);
+	const previousButton = container.querySelector('.tab-previous') as HTMLButtonElement;
+	const nextButton = container.querySelector('.tab-next') as HTMLButtonElement;
+
+	if (index <= 1) {
+		previousButton.classList.add('display-none');
+		nextButton.classList.remove('display-none');
+	} else if (index >= buttons.length) {
+		nextButton.classList.add('display-none');
+		previousButton.classList.remove('display-none');
+	} else {
+		previousButton.classList.remove('display-none');
+		nextButton.classList.remove('display-none');
 	}
 }
 
@@ -48,19 +73,15 @@ function initTabControlClickListeners(container: HTMLElement = document.body) {
 		button.addEventListener('click', (e: Event) => {
 			e.preventDefault();
 			const b = e.target as HTMLButtonElement;
-			const parentElement = b.closest('.tab-parent') as HTMLElement;
 
-			if (!parentElement) {
-				return;
-			}
-
-			updateTabState(container, parentElement);
+			updateTabState(container, b.dataset.tabControl as string);
 			updateTabItemState(container, b.dataset.tabControl as string);
+			updateTabNav(container);
 		});
 	}
 }
 
-function updateTabState(container: HTMLElement, parentElement: HTMLElement) {
+function updateTabState(container: HTMLElement, activatedTab: string) {
 	// remove is current from all anchors in this container
 	const tabParents = Array.from(container.querySelectorAll('.tab-parent')) as HTMLElement[];
 
@@ -70,13 +91,16 @@ function updateTabState(container: HTMLElement, parentElement: HTMLElement) {
 	for (const tp of tabParents) {
 		tp.classList.remove('is-active');
 	}
+
 	// add one back
+	const updatedTab = container.querySelector(`[data-tab-control="${activatedTab}"]`) as HTMLElement;
+	const parentElement = updatedTab.closest('.tab-parent') as HTMLElement;
 	parentElement.classList.add('is-active');
 }
 
 function updateTabItemState(container: HTMLElement, activatedTab: string) {
 	const tabItemElements = Array.from(container.querySelectorAll('[data-tab-item')) as HTMLElement[];
-	const tabItem = container.querySelector(`[data-tab-item=tab-item-${activatedTab}`);
+	const tabItem = container.querySelector(`[data-tab-item="tab-item-${activatedTab}"]`);
 
 	if (!tabItemElements) {
 		return;
