ef7da3dacd6636a1244c6180fd0bb18bf9ae1c5d
Olga Lipskaya <ollipska@microsoft.com>
2023-06-02 12:30:23 -0700

    Final final fix in test

--- integration/tests/dismiss.spec.ts
+++ integration/tests/dismiss.spec.ts
@@ -4,14 +4,12 @@ test('dismiss removes parent component from the DOM after clicked', async ({ pag
 	await page.goto('/components/dismiss.html');
 
 	// targeting notification component
-	const notificationEl = page.locator('#example-dismiss-01');
-	expect(notificationEl).toHaveCount(1);
-	expect(notificationEl).toHaveAttribute('data-dismissable', '');
-	expect(notificationEl).toHaveClass('notification');
+	const dismissableEl = page.locator('#example-dismiss-01');
+	expect(dismissableEl).toHaveCount(1);
+	expect(dismissableEl).toHaveAttribute('data-dismissable', '');
 
-	const dismissEl = notificationEl.locator('[data-dismiss]');
+	const dismissEl = dismissableEl.locator('[data-dismiss]');
 	expect(dismissEl).toHaveAttribute('type', 'button');
-	expect(dismissEl).toHaveClass('notification-dismiss');
 
 	await dismissEl.click();
 
