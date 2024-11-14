1657e490d21ed9f7e12e8b1afcaeaa06fbc8deba
Will Bjorn <wibjorn@microsoft.com>
2024-11-14 11:02:29 -0800

    update

--- integration/tasks/action.ts
+++ integration/tasks/action.ts
@@ -1,5 +1,6 @@
+// https://github.com/actions/toolkit/tree/main
+//https://github.com/actions/toolkit/tree/main/packages/artifact
+// https://www.npmjs.com/package/@actions/github
 import * as core from '@actions/core';
 
-console.log('action');
-
-core.notice('Something happened that you might want to know about.');
+core.notice('Something happened that you might want to know about.', {});
